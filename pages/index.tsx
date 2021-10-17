5b884e1679137faa7e67a19781bf8d3586849b7a
jideabdqudus <jideabdqudus@gmail.com>
2021-10-18 00:18:32 +0100

    Deployment of CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,39 +19,3 @@ export default function Home({launches}) {
     </div>
   );
 }
-
-
-// export async function getStaticProps() {
-//   const client = new ApolloClient({
-//     uri: 'https://api.spacex.land/graphql/',
-//     cache: new InMemoryCache()
-//   });
-//   const { data } = await client.query({
-//     query: gql`
-//       query GetLaunches {
-//         launchesPast(limit: 10) {
-//           id
-//           mission_name
-//           launch_date_local
-//           launch_site {
-//             site_name_long
-//           }
-//           links {
-//             article_link
-//             video_link
-//             mission_patch
-//           }
-//           rocket {
-//             rocket_name
-//           }
-//         }
-//       }
-//     `
-//   });
-//   console.log(data, "data")
-//   return {
-//     props: {
-//       launches: []
-//     }
-//   }
-// }
